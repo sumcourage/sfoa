06be8113553af90cf99529e88624eef14b950347
yaya <15686143552@163.com>
2018-10-17 00:51:52 +0800

    创建项目

---
+++ f1_client/src/components/HelloWorld.vue
@@ -0,0 +1,28 @@
+<template>
+
+   <ul>
+     <li>糖糖</li>
+     <li>丫丫</li>
+     <li>糖宝</li>
+     <li>{{msg}}</li>
+   </ul>
+</template>
+
+<script>
+export default {
+  name: 'HelloWorld',
+  data () {
+    return {
+      msg: 'Welcome to Your Vue.js App'
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+</style>
